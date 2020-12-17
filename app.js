6d673374a27235b91af39308ce1343c76d7a1855
Abu Raihan Rony <44436797+ronyaburaihan@users.noreply.github.com>
2020-12-17 23:46:42 +0600

    Single note reading functionality added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const chalk = require('chalk')
 const notes = require('./notes')
 const yargs = require('yargs')
+const { argv } = require('yargs')
 
 //customize yargs version
 yargs.version('1.1.0')
@@ -55,8 +56,15 @@ yargs.command({
 yargs.command({
     command: 'read',
     describe: 'Read a note',
-    handler(){
-        console.log('Reading a note')
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv){
+        notes.readNote(argv.title)
     }
 })
 
--- notes.js
+++ notes.js
@@ -1,16 +1,12 @@
 const chalk = require('chalk')
 const fs = require('fs')
 
-const getNotes = () => {
-    return 'Your notes .....'
-}
-
 const addNote = (title,body) => {
     const notes = loadNotes()
 
-    const duplicateNotes = notes.filter((note) => note.title === title)
+    const isNoteDuplicate = notes.find( (note) => note.title === title)
 
-    if(duplicateNotes.length === 0){
+    if(!isNoteDuplicate){
         notes.push({
             title: title,
             body: body
@@ -60,9 +56,22 @@ const listNotes = () => {
     notes.forEach( (note) => console.log(note.title))
 }
 
+const readNote = (title) => {
+    const notes = loadNotes()
+
+    const note = notes.find((note) => note.title === title)
+
+    if(note){
+        console.log(chalk.white.inverse(note.title))
+        console.log(note.body)
+    } else {
+        console.log(chalk.red.inverse('Note not found!'))
+    }
+}
+
 module.exports = {
-    getNotes: getNotes,
     addNote: addNote,
     removeNote: removeNote,
-    listNotes: listNotes
+    listNotes: listNotes,
+    readNote: readNote
 }
